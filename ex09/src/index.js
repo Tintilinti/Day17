72bf895098acffb56b6ed2a0e2b575ad82bff0cb
Bruno Lovric <brunoolovric@gmail.com>
2021-03-25 10:58:03 +0100

    added find person by id project

---
+++ ex09/src/index.js
@@ -0,0 +1,46 @@
+const _ = require('lodash');
+var users = [
+    {
+        id: 1,
+        firstName: "John",
+        lastName: "Doe",
+        age: 24,
+        gender: "male"
+    },
+    {
+        id: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 5,
+        gender: "female"
+    },
+    {
+        id: 3,
+        firstName: "Jim",
+        lastName: "Carrey",
+        age: 54,
+        gender: "male"
+    },
+    {
+        id: 4,
+        firstName: "Kate",
+        lastName: "Winslet",
+        age: 40,
+        gender: "female"
+    }
+];
+function getUsers() {
+    var output = ""
+    return output;
+}
+function findUserById(id) {
+    try {
+
+    } catch (error) {
+        return;
+        console.log();
+    }
+}
+getUsers();
+findUserById();
+module.exports = findUserById;
